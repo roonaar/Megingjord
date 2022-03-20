e655debdc4a4ffa5f9e224695a71a72aa2a8c5ec
roonaar <runarsook@gmail.com>
2022-03-20 15:42:19 +0100

    Lansert nytt innhold

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -50,10 +50,8 @@ export function SlideShow(id) {
     async function check_for_loaded_icons() {
         const done = document.documentElement.classList.contains('fontawesome-i2svg-complete');
         if (done) {
-            console.log('done')
             return Promise.resolve(done);
         } else {
-            console.log('retry');
             await new Promise(resolve => setTimeout(resolve, 5));
             return check_for_loaded_icons();
         }
